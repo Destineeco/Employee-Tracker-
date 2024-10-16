09f5d80fb7af15886220e04929bcef14dfd8f369
HP USER <Destmil.co@gmail.com>
2024-10-16 19:41:08 -0400

    added inserts to schema and query

---
+++ db/query.sql
@@ -0,0 +1,11 @@
+
+SELECT
+    department.title AS department,
+    employee.first_name,
+    employee.last_name,
+    role.title
+FROM
+    employee
+LEFT JOIN role ON employee.role_id = role.id
+LEFT JOIN department ON role.department_id = department.id
+ORDER BY department.title, employee.last_name;
---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (department_title) 
+VALUES ('inbound'),
+       ('outbound');
+
+INSERT INTO role (department_id, title, salary)
+VALUES (1, 'unloader', 40000.00),
+       (1, 'scanner', 30000.00),
+       (1, 'driver', 50000.00),
+       (2, 'loader', 40000.00),
+       (2, 'scanner', 30000.00),
+       (2, 'driver', 50000.00);
+        
+INSERT INTO employee ( first_name, last_name, role_id, manager_id) 
+VALUES ('Nicole','Anders', 101, 10),
+       ('Nick','Con', 102, 10),
+       ('Cole','johnson', 103, 10).
+       ('Rick','Owens', 201, 20),
+       ('Jen','Rogers', 202, 20),
+       ('Brandon','Williams', 203, 20);
+
